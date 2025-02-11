6e5b8944da03d63f2361b2d3ef7a256a06682934
sugalex <banmann87@gmail.com>
2025-02-11 20:04:08 +0100

    Delete The real Lost World directory

--- The real Lost World/Spieler.py
+++
@@ -1,9 +0,0 @@
-class Spieler:
-
-    def __init__(self):
-        self.position = 0
-        self.max_schritte = 11
-
-    def vorwaerts_gehen(self):
-        self.position += 1
-        print(f"Der Spieler ist ein Schritt nach vorne gegangen. Position {self.position}")
